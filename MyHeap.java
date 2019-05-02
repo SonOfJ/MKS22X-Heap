fd17fc7f9d04c401c8dfa0f04c0156c2ff312de9
Jason Zheng <zzheng3@stuy.edu>
2019-05-01 23:23:24 -0400

    Should be done with pushDown.

--- MyHeap.java
+++ MyHeap.java
@@ -3,13 +3,25 @@ public class MyHeap {
     if (size > index * 2 + 1) { //There will only be pushing when a new row is needed.
       if (size != index * 2 + 2) { //If the final row isn't complete.
         if (data[index * 2 + 1] > data[index * 2 + 2]) { //If the left value is greater than the right value...
-          int hold = data[index]; //Swap the left value with the upper value.
-          data[index] = data[index * 2 + 1];
-          data[index * 2 + 1] = hold;
-          pushDown(data, size, index * 2 + 1); //Move on the the bottom left value.
+          if (data[index * 2 + 1] > data[index]) { //Only if the bottom value is bigger than the top value...
+            int hold = data[index]; //Swap the left value with the upper value.
+            data[index] = data[index * 2 + 1];
+            data[index * 2 + 1] = hold;
+            pushDown(data, size, index * 2 + 1); //Move on to the the bottom left value.
+          }
+        } else if (data[index * 2 + 2] > data[index]) { //If the right value is bigger, check to see if it is bigger than the top value.
+          int hold = data[index]; //Swap the right value with the upper value.
+          data[index] = data[index * 2 + 2];
+          data[index * 2 + 2] = hold;
+          pushDown(data, size, index * 2 + 2); //Move on to the bottom right value.
         }
+      } else if (data[index * 2 + 1] > data[index]) { //If the final row is complete and the bottom left value is bigger than the top value...
+        int hold = data[index]; //Simply swap the two.
+        data[index] = data[index * 2 + 1];
+        data[index * 2 + 1] = hold;
       }
     }
+  }
   private static void pushUp(int[]data,int index)
   public static void heapify(int[])
   public static void heapsort(int[])
