3f4da6ae3b9a624ca19e9c0ad8ba0a8636af8a0b
Jason Zheng <zzheng3@stuy.edu>
2019-05-01 22:58:20 -0400

    Started on pushDown.

--- MyHeap.java
+++ MyHeap.java
@@ -1,5 +1,9 @@
 public class MyHeap {
-  private static void pushDown(int[]data,int size,int index)
+  private static void pushDown(int[]data, int size, int index) {
+    if (size > index * 2 + 1) { //There will only be pushing when necessary.
+
+    }
+  }
   private static void pushUp(int[]data,int index)
   public static void heapify(int[])
   public static void heapsort(int[])
